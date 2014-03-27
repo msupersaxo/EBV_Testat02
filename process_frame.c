ee81a8500723d11d66bf3a92107539a36b8c365e
oscar <oscar@oscar.(none)>
2014-03-27 18:32:02 +0100

    first version change detection

--- process_frame.c
+++ process_frame.c
@@ -12,15 +12,73 @@
 #include <string.h>
 #include <stdlib.h>
 
-OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn, struct OSC_VIS_REGIONS *regions, uint8 Color);
+const int MaxCounter = 50;
+
 
 void ProcessFrame(uint8 *pInputImg)
 {
+	int c, r, i0;
+	int nc = OSC_CAM_MAX_IMAGE_WIDTH/2;/* we work on half of the camera image width */
 	int siz = sizeof(data.u8TempImage[GRAYSCALE]);
+	for(r = nc; r < siz-nc; r+= nc)/* we skip the first and last line */
+	{
+		for(c = 1; c < nc-1; c++)
+		{
+			if(data.ipc.state.nStepCounter == 1) {//initialize counters
+				for(i0 = 0; i0 < 3; i0++) {
+					 data.u8TempImage[INDEX0+i0][r+c] = 0;
+				}
+			}
+			/* do pointer arithmetics with respect to center pixel location */
+			unsigned char* p = &data.u8TempImage[GRAYSCALE][r+c];
+			/* implement Sobel filter (shift by 128 to 'center' grey values */
+			short dx =     -(short) *(p-nc-1) + (short) *(p-nc+1)
+			            -2* (short) *(p-1) + 2* (short) *(p+1)
+			               -(short) *(p+nc-1) + (short) *(p+nc+1);
+			/* apply min()/max() to avoid wrap around of values below 0 and above 255 */
 
-	memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[GRAYSCALE], siz);
+			/* implement Sobel filter (shift by 128 to 'center' grey values */
+			short dy =    -(short) *(p-nc-1) -2* (short) *(p-nc) - (short) *(p-nc+1)
+			               +(short) *(p+nc-1) +2* (short) *(p+nc) + (short) *(p+nc+1);
+			/* apply min()/max() to avoid wrap around of values below 0 and above 255 */
+			short absdx = abs(dx);
+			short absdy = abs(dy);
 
-	memset(data.u8TempImage[THRESHOLD], 0, siz);
+			unsigned char Edge = (absdx+absdy) < data.ipc.state.nThreshold ? 0 : 255;
+
+			int Index = 0;
+			if(Edge) {  //edge is true
+				if(absdx > absdy) {
+					Index = 1;
+				} else {
+					Index = 2;
+				}
+			} //else nothing to do (Index already = 0)
+
+			//increment counters
+			int MaxV = data.u8TempImage[INDEX0][r+c];
+			int MaxI = 0;
+			for(i0 = 0; i0 < 3; i0++) {
+				if(Index == i0) {//increment corresponding counter
+					data.u8TempImage[INDEX0+i0][r+c] = MIN(MaxCounter, data.u8TempImage[INDEX0+i0][r+c]+1);
+				} else {//decrement corresp. counter
+					data.u8TempImage[INDEX0+i0][r+c] = MAX(0, data.u8TempImage[INDEX0+i0][r+c]-1);
+				}
+
+				if(data.u8TempImage[INDEX0+i0][r+c] > MaxV) {
+					MaxV = data.u8TempImage[INDEX0+i0][r+c];
+					MaxI = i0;
+				}
+			}
+			//condition for foreground
+			data.u8TempImage[THRESHOLD][r+c] = MaxV;
+			data.u8TempImage[BACKGROUND][r+c] = (MaxV > MaxCounter/2) && (Index != MaxI) ? 255 : 0;
+		}
+	}
+
+
+
+
 }
 
 
